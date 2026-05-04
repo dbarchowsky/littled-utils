fbc715eda6462e711ce0dd2c2c9781160bb07617
Damien Barchowsky <dbarchowsky@gmail.com>
2026-05-04 16:38:40 -0700

    Added test of `deepMerge` to confirm source objects are not altered

--- __tests__/deepMerge.test.js
+++ __tests__/deepMerge.test.js
@@ -11,4 +11,16 @@ describe('deepMerge', () => {
         expect(result).toEqual(data.expected);
         expect(data.target).toEqual(data.expected);
     });
+
+    test("Should alter target. Should not alter the source objects. Should return a copy.", () => {
+       const target = {a: 1, b: 2};
+       const source1 = {b: 3, c: 4};
+       const source2 = {d: 5};
+
+       const result = deepMerge(target, source1, source2);
+       expect(result).toEqual({a: 1, b: 3, c: 4, d: 5});
+       expect(target).toEqual({a: 1, b: 3, c: 4, d: 5});
+       expect(source1).toEqual({b: 3, c: 4});
+       expect(source2).toEqual({d: 5});
+    });
 });
